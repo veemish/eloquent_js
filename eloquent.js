e4bf6f3eed43da22b029cd1c5f5e0bcb13ab57bc
veemish <vlikunama@gmail.com>
2020-02-06 11:33:43 +0300

    Robot exercise 3 solution

--- eloquent.js
+++ eloquent.js
@@ -51,3 +51,35 @@ function countSteps(state, robot, memory) {
   }
   
   runRobotAnimation(VillageState.random(), lazyRobot, []);
+
+  /* Exercise 3 Persistent group*/
+
+  class PGroup {
+    constructor(members) {
+      this.members = members;
+    }
+    add(value){
+      if (this.has(value)) return this;
+      return new PGroup(this.members.concat([value]))
+    }
+    delete(value){
+      if(!this.has(value))return this;
+      return new PGroup(this.members.filter(m => m !== value));
+    }
+    has(value){
+      return this.members.includes(value)
+    }
+  }
+
+  PGroup.empty = new PGroup.empty.add("a");
+  let ab = a.add("b");
+  let b = ab.delete("b");
+
+  console.log(b.has("b"));
+  //-> true
+
+  console.log(a.has("b"));
+  // -> false
+
+  console.log(b.has("a"));
+  // -> false
