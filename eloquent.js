d8556a66745b5124899775261a863919389355cf
veemish <vlikunama@gmail.com>
2020-02-10 11:59:18 +0300

    add chapter 6

--- eloquent.js
+++ eloquent.js
@@ -83,3 +83,65 @@ function countSteps(state, robot, memory) {
 
   console.log(b.has("a"));
   // -> false
+
+
+  //Chapter 6
+  //Exercise 1 A vector type
+
+  class Vec{
+    constructor(x,y){
+      this.x = x;
+      this.y = y;
+    }
+    plus(other){
+      return new Vec(this.x + other.x , this.y + other.y );
+    }
+    minus(other) {
+      return new Vec(this.x + other.x , this.y - other.y);
+    }
+    get length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+  }
+
+  console.log(new Vec(1 , 2).plus(new Vec(2 , 3)));
+  //Vec{X:3 , y:5}
+  console.log(new Vec(1,2).minus(new Vec(2,3)));
+  //Vec{x: -1 , Y: -1}
+  console.log(new Vec(3,4).length);
+  //5
+
+  //exercise 2 Groups:
+  class Group {
+    constructor(){
+      this.members = [];
+    }
+    add(value){
+      if(!this.has(value)){
+        this.members.push(value);
+      }
+    }
+    delete(value){
+      this.members = this.members.filter(v => v !== value);
+    }
+    has(value){
+      return this.members.includes(value);
+    }
+    static from(collection){
+      let group = new Group;
+      for (let value of collection){
+        group.add(value);
+      }
+      return group;
+    }
+  }
+
+  let group = Group.from([10, 20]);
+  console.log(group.has(10));
+  //true
+  console.log(group.has(30));
+  // false
+
+  group.add(10);
+  group.delete(10);
+  console.log(group.has(10));
