9f95106a51b88dead868e6ca64021c5c26175422
veemish <vlikunama@gmail.com>
2020-02-11 14:34:33 +0300

    add chapter 6

--- eloquent.js
+++ eloquent.js
@@ -144,4 +144,68 @@ function countSteps(state, robot, memory) {
 
   group.add(10);
   group.delete(10);
-  console.log(group.has(10));
+  console.log(group.has(10)); 
+
+  //Exercise 3 Iterable groups
+
+  class Group {
+    constructor() {
+      this.members = [];
+    }
+    add(value) {
+      if (!this.has(value)){
+        this.members.push(value);
+      }
+    }
+    delete(value){
+      this.members = this.members.filter( v => v !== value)
+    }
+    has(value){
+      return this.members.includes(value);
+    }
+    static form(collection){
+      let group = new Group;
+      for (let value of collection){
+        group.add(value);
+      }
+      return group;
+    }
+    [Symbol.iterator]() {
+      return new GroupIterator(this);
+    }
+  }
+
+  class GroupIterator {
+    constructor(group){
+      this.group = group;
+      this.position = 0;
+    }
+
+    next() {
+      if(this.position >= this.group.members.length){
+        return {done: true};
+      } else {
+        let result = {value : this.group.members[this.position],
+        done:false};
+        this.position++;
+        return result;
+      }
+     
+    }
+  
+  }
+
+  for (let value of Group.from(["a" ,"b" , "c"])){
+    console.log(value);
+  }
+  // a
+  //b
+  //c
+
+  //BORROWING  A METHOD
+  let map = {
+    one: true, two: true, hasOwnProperty: true
+  };
+
+  log(Object.prototype.hasOwnProperty.call(map, "one"));
+  //true
