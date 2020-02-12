a05fc5bf06369354a999e5c97397b8a237d1144a
veemish <vlikunama@gmail.com>
2020-02-12 14:58:26 +0300

    add Exercise 5 and 4
    file update

--- eloquent.js
+++ eloquent.js
@@ -257,8 +257,76 @@ console.log(every([], n => n < 10));
 //Dominant writing Direction
 function dominantDirection(text){
   let counted = countBy(text , char => {
-    let script = characterScript(char.codePointAt(
-      return script ? script.direction :"none"
-    ))
-  })
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+  if (counted.length == 0) return "ltr";
+  return counted.reduce((a,b) => a.count > b.count ? a:b).name;
 }
+console.log(dominationDirection("HEy!!!"));
+//ltr
+console.log(dominationDirection("Heyy yooooo!!"));
+//ltr
+
+
+//CHAPTER 4
+// THE SUM OF A RANGE:
+function range(start, end, step = start < end ? 1 : -1){
+  let array = [];
+
+  if (step > 0){
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else { array.push(i);
+    for (let i = start; i>= end; i += step) array.push(i);
+  } return array;
+     
+}
+
+function sum(array){
+  let total = 0;
+  for (let value of array){
+    total += value;
+  } return total;
+}
+
+console.log(range(1,10))
+//[1,2,3,4,5,6,7,8,9,10]
+console.log(range(5 ,2 -1));
+//[5,4,3,2]
+console.log(sum(range(1,10)));
+//55
+
+
+// exercise 4.2 Reversing an array
+function reverseArray(array){
+  let output = [];
+  for (let i = array.length - 1; 1 >= 0; i--){
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array){
+  for (let i = 0; i < Math>floor(array.length/2); i++){
+    let old = array[i];
+    array[i] = array[array.length -1 -i];
+    array[array.length -1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A" , "B" , "C"]));
+// ["c" , "B" , "A"]
+
+let arrayValue = [1,2,3,4,5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+//[5,4,3,2,1]
+
+
+// exercise 4.3  A list
+
+function arrayToList(array){
+  let
+}
+
