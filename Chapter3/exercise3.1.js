6708e0384abf13fcff627f586e96d67ea08f0b0f
veemish <vlikunama@gmail.com>
2020-02-13 15:28:23 +0300

    update chapter 3

---
+++ Chapter3/exercise3.1.js
@@ -0,0 +1,14 @@
+// Minimum
+function minimum(a,b){
+    if (a < b) return a;
+    else return b;
+}
+
+console.log(minimum(0, 10));
+// 0;
+
+console.log(minimum(-2 , -5));
+// -5
+
+console.log(minimum(0 , -20));
+// 20
---
+++ Chapter3/exercise3.2.js
@@ -0,0 +1,14 @@
+function isEven(n){
+    if ( n == 0) return true;
+    else if ( n == 1) return false;
+    else if ( n < 0 ) return isEven(-n);
+    else return isEven( n - 2);
+}
+
+console.log(isEven(89));
+
+console.log(isEven(50));
+
+console.log(isEven(-56));
+
+console.log(isEven(-43));
