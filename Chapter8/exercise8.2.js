4a7a27ba5f5a2887bd8df25f0b6bf9e87dc06315
veemish <vlikunama@gmail.com>
2020-02-20 13:26:52 +0300

    update chapter 8

--- Chapter8/exercise8.2.js
+++ Chapter8/exercise8.2.js
@@ -4,5 +4,38 @@ const box = {
         this.locked = false;
     },
     lock(){ this.locked = true;},
-    _
+    _content: [],
+    get content(){
+        if (this.locked) throw new Error("LOCKED!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body){
+    let locked = box.locked;
+    if (!locked){
+        return body();
+    }
+
+    box.unlock();
+    try {
+        return body();
+    } finally {
+        box.lock();
+    }
 }
+
+withBoxUnlocked(function(){
+    box.content.push("gold flower");
+});
+
+ try {
+     withBoxUnlocked(function(){
+         throw new Error("Pirate on the horizon! Abort! ")
+     });
+ } catch (e){
+     console.log("Error raised:", e);
+ }
+
+ console.log(box.locked);
+ // true
